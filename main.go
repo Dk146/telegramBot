61f1d325393a9a1a0479883fca4635fa644ebb0c
khanh.nguyen3 <khanh.nguyen3@BeF-LMA2020M1-A279.local>
2022-11-28 14:37:25 +0700

    add env variables

--- main.go
+++ main.go
@@ -7,94 +7,58 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 	"text/template"
 
 	"cloud.google.com/go/pubsub"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var listChatID []int64
-
 func main() {
 	ctx := context.Background()
-	go handleCommand()
 
-	f, err := ioutil.ReadFile("default.tmpl")
-	if err != nil {
-		log.Print(err)
-		return
-	}
+	bot_token := os.Getenv("BOT_TOKEN")
+	template_path := os.Getenv("TEMPLATE")
+	pubsub_project_id := os.Getenv("PUBSUB_PROJECT_ID")
+	pubsub_sub_id := os.Getenv("PUBUB_SUB_ID")
+	chat_id := os.Getenv("CHAT_ID")
 
-	bot, err := tgbotapi.NewBotAPI("5849872871:AAEtgwHnnXURk9dTttUEIRe26C-_EW6Kq60")
+	f, err := ioutil.ReadFile(template_path)
 	if err != nil {
 		log.Panic(err)
 	}
-	client, err := pubsub.NewClient(ctx, "my-project-1668668140794")
+	bot, err := tgbotapi.NewBotAPI(bot_token)
 	if err != nil {
-		// TODO: Handle error.
+		log.Panic(err)
 	}
-
-	sub := client.Subscription("sub_one")
+	client, err := pubsub.NewClient(ctx, pubsub_project_id)
+	if err != nil {
+		log.Panic(err)
+	}
+	sub := client.Subscription(pubsub_sub_id)
 
 	err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
 		t := template.Must(template.New("").Parse(string(f)))
 		in := []byte(string(m.Data))
+		fmt.Println(string(m.Data))
+
 		var raw map[string]interface{}
 		if err := json.Unmarshal(in, &raw); err != nil {
-			panic(err)
+			fmt.Println(err)
 		}
 		var tpl bytes.Buffer
 		if err := t.Execute(&tpl, raw); err != nil {
-			panic(err)
+			fmt.Println(err)
 		}
-		fmt.Println(listChatID)
-		for _, id := range listChatID {
-			fmt.Println(id)
-			msg := tgbotapi.NewMessage(int64(id), tpl.String())
-			bot.Send(msg)
-		}
+
+		int_chat_id, _ := strconv.Atoi(chat_id)
+		msg := tgbotapi.NewMessage(int64(int_chat_id), tpl.String())
+		bot.Send(msg)
 		m.Ack()
 	})
 
 	if err != context.Canceled {
-		// TODO: Handle error.
-	}
-}
-
-func handleCommand() {
-	bot, err := tgbotapi.NewBotAPI("5849872871:AAEtgwHnnXURk9dTttUEIRe26C-_EW6Kq60")
-	if err != nil {
-		panic(err)
-	}
-
-	bot.Debug = true
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		fmt.Println("Hello")
-		if update.Message == nil {
-			continue
-		}
-
-		if !update.Message.IsCommand() {
-			continue
-		}
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
-		switch update.Message.Command() {
-		case "start":
-			listChatID = append(listChatID, int64(update.Message.Chat.ID))
-			fmt.Println(listChatID)
-			msg.Text = "I understand /sayhi."
-		default:
-			msg.Text = "I don't know that command"
-		}
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
+		log.Panic(err)
 	}
 }
